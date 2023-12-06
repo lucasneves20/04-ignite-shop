2ec0a101446934e3a6a4faaa1ff1aa1bc8b7f6d5
Lucas Neves <heraldofdevs@gmail.com>
2023-12-05 22:34:21 -0300

    Modulo 4 aula 16 Produto e Checkout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
+import { globalStyles } from '@/styles/globals'
+import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import logoImg from "@/assets/Logo.svg"
+import { Container, Header } from '@/styles/pages/app'
+
+
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt='Logo do Ignite Shop'/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
